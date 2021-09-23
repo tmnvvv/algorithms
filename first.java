9cca411450e1dfcc897e8281b606d196c666295d
Hopes9 <62842164+Hopes9@users.noreply.github.com>
2021-09-23 10:53:51 +0300

    Update first.java

--- first.java
+++ first.java
@@ -1,22 +1,22 @@
 import java.util.Arrays;
 
-public class first {
+public class Recurtion {
 
 
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5, 6, 7};
 
-        myReverse(numbers, 8, 0);
+        recurtion(numbers, 8, 0);
     }
 
-    public static int[] myReverse(int[] array, int count, int index) {
+    public static int[] recurtion(int[] array, int count, int index) {
         for (int i = 0; i < array.length; i++) {
             if (i != index) {
                 if ((array[i] +  array[index]) == count) {
                     System.out.println("1: " + array[i] + " 2: " + array[index] + " count = " + (array[i] + array[index]));
                     break;
                 } else {
-                    myReverse(array, count, index + 1);
+                    recurtion(array, count, index + 1);
                 }
             }
         }
