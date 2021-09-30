eb6696c9b64a296d5abf109aea61c7d05bc4de1c
corruptzero-dev <74492746+corruptzero-dev@users.noreply.github.com>
2021-09-30 11:33:42 +0300

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,40 +1,71 @@
 package hi;
 
-import java.util.Scanner;
-
 public class Book {
-    static String title = Title.show();
-    static String author = Author.show();
-    static String content = Content.show();
+    private Title title;
+    private Author author;
+    private Content content;
+
+    public Book(String title, String author, String content) {
+        this.title = new Title(title);
+        this.author = new Author(author);
+        this.content = new Content(content);
+    }
+
+//    public Book(Title title, Author author, Content content) {
+//        this.title = title;
+//        this.author = author;
+//        this.content = content;
+//    }
+
+
+    public void show() {
+        System.out.print("Книга с названием: ");
+        this.title.show();
+        System.out.print("Автор: ");
+        this.author.show();
+        System.out.print("Содержание: ");
+        this.content.show();
+    }
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите название: ");
-        Title.title = scanner.next();
-        System.out.println();
-        System.out.print("Введите автора: ");
-        Author.author = scanner.next();
-        System.out.println();
-        System.out.print("Введите содержание: ");
-        Content.content = scanner.next();
-        System.out.println();
-        System.out.println("Название: "+ Title.show() + "\nАвтор: " + Author.show() + "\nСодержание: " + Content.show());
+        Book book = new Book("Совершенный код","Стив Макгонал","...");
+        book.show();
+
     }
-    static class Title{
-        static String title = "";
-        public static String show() {
-            return(title);
-        }
+}
+
+class Title {
+    private String title;
+
+    public Title(String title) {
+        this.title = title;
     }
-    static class Author{
-        static String author = "";
-        public static String show() {
-            return(author);
-        }
+
+    public void show() {
+        System.out.println(this.title);
     }
-    static class Content{
-        static String content = "";
-        public static String show() {
-            return(content);
-        }
+}
+
+class Author {
+    private String author;
+
+    public Author(String author) {
+        this.author = author;
+    }
+
+    public void show() {
+        System.out.println(this.author);
+    }
+}
+
+class Content {
+    private String content;
+
+    public Content(String content) {
+        this.content = content;
+    }
+
+    public void show() {
+        System.out.println(this.content);
     }
 }
